b0d0cf826199dbd3c73c6402c434ac6d6669f5ae
Hackbright Student <info@hackbrightacademy.com>
2014-03-04 21:35:41 -0800

    before my if statements get messed up

--- api.py
+++ api.py
@@ -15,34 +15,51 @@ def index():
 
 @app.route('/', methods=['POST'])
 def index_post():
+    # create a new list
     list_name = request.form.get('todo_list_name')
-    if not list_name:
-        flash("I'm sorry, but you'll need to specify a list name")
 
-    else:
-        todo_lists.insert(dict(list_name=list_name))
-        flash('Todo list %s created' % list_name)
-    return redirect('/')
+    # ASK WHAT IS HAPPENING.
+    todo_lists.insert(dict(list_name=list_name))
 
-# handler for asynchronous request
-# @app.route('/ajax/todo_list', methods=['POST'])
-# def ajax_todo_list():
-#     results = todo_lists.all()
-#     lists = [l for l in results]
-#     return render_template("todo_list_partial.html", lists=lists)
+    # return all of the lists (including our new one)
+    results = todo_lists.all()
+    lists = [l for l in results]
+    return render_template("todo_list_partial.html", lists=lists)
+
+
+    # FLASHING AND AJAX DOESN'T WORK.
+
+    # if not list_name:
+    #     flash("Please specify a list name.")
+    #     return render_template("todo_list_partial.html", lists=lists)
+    # else:
+    #     # ASK WHAT IS HAPPENING.
+    #     todo_lists.insert(dict(list_name=list_name))
+
+    #     # return all of the lists (including our new one)
+    #     results = todo_lists.all()
+    #     lists = [l for l in results]
+    #     flash('Todo list %s created' % list_name)
+    #     return render_template("todo_list_partial.html", lists=lists)
 
 @app.route('/todo_lists/<int:id>')
 def todo_list_show(id):
-    items = todo_item_table.find(todo_list_id=id)
-    items = [x for x in items]
-
+    results = todo_item_table.find(todo_list_id=id)
+    items = [x for x in results]
     return render_template("todo_list.html", items=items)
 
 @app.route('/todo_lists/<int:id>', methods=["POST"])
 def todo_item_create(id):
-    resp = todo_item_table.insert(dict(task=request.form.get("task"), todo_list_id=id, done=False))    
+    # create a new todo_item, insert into database
+    todo_item_table.insert(dict(task=request.form.get("task"), todo_list_id=id, done=False))
 
-    return redirect("/todo_lists/%d" % id)
+    # return all of the items for a given list
+    # ASK WHAT IS THIS MAGIC OF .find? 
+    results = todo_item_table.find(todo_list_id=id)
+    items = [x for x in results]
+    return render_template("todo_items_partial.html", items=items)
+
+    # return redirect("/todo_lists/%d" % id)
 
 
 @app.route('/todo_lists/poll')
@@ -50,6 +67,14 @@ def poll_for_lists():
     results = todo_lists.all()
     lists = [l for l in results]
     return render_template("todo_list_partial.html", lists=lists)
+
+
+# handler for asynchronous request
+# @app.route('/ajax/todo_list', methods=['POST'])
+# def ajax_todo_list():
+#     results = todo_lists.all()
+#     lists = [l for l in results]
+#     return render_template("todo_list_partial.html", lists=lists)
 
 
 if __name__ == '__main__':
--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-    var formSubmitButton = $("#form_submit");
+    var listSubmitButton = $("#list_form_submit");
+
+    if (listSubmitButton !== null) {
+    }
 
     formSubmitButton.on("click", function(event) {
         event.preventDefault(); // prevent the browser form submission from happening
@@ -8,27 +11,59 @@ $(document).ready(function(){
             method: "POST",
             data: $("form#todo_list_form").serialize(),
         }).done(function(responseData){
-            // var listElements = document.getElementById('todo-list');
-            // listElements.innerHTML = responseData;
+            // lists
+            var formElement = document.getElementById("todo_list_id");
+            formElement.value = '';
+            document.getElementById("todo-list").innerHTML = responseData;
             console.log(responseData);
         }).fail(function(){
             console.log('fail!!!');
         });
+
+
+    var itemSubmitButton = $("#item_form_submit");
+    itemSubmitButton.on("click", function(event) {
+        event.preventDefault();
+        $.ajax({
+            url: "/todo_lists/<int:id>",
+            method: "POST",
+            data: $("form#todo-item-form").serialize(),
+        }).done(function(responseData){
+            // list to-do items -- will these overlap/be the same as above?
+            // var formElement = document.getElementById("todo_list_id");
+            // formElement.value = '';
+            // document.getElementById("todo-list").innerHTML = responseData;
+
+            console.log(responseData);
+            
+        }).fail(function(){
+            console.log('item ajax failed!!!');
+        });
     });
 
-    var getListsFromServer = function() {
-        $.ajax({
-            url: '/todo_lists/poll',
-            method: "GET"
-        }).done(function(data){
-            var toDoList = document.getElementById("todo-list");
-            var newList = data;
-            toDoList.innerHTML = newList;
-            console.log(data);
-        });
-    };
 
-    setInterval(getListsFromServer, 2000);
+
+
+    });
+
+    // var getListsFromServer = function() {
+    //     $.ajax({
+    //         url: '/todo_lists/poll',
+    //         method: "GET"
+    //     }).done(function(data){
+    //         // reset "Create New List" field to a blank value
+    //         var formElement = document.getElementById("todo_list_id");
+    //         formElement.value = '';
+    //         // update displayed HTML
+    //         var toDoList = document.getElementById("todo-list");
+    //         var newList = data;
+    //         toDoList.innerHTML = newList;
+    //         console.log(data);
+    //     });
+    // };
+
+    // polling the server
+    // setInterval(getListsFromServer, 5000);
 
 
 
